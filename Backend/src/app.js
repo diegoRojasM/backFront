8f4d65523b31efa33d7542aab8fb4888676a661f
diegoRojasM} <diegohelfo@gmail.com>
2024-11-06 13:46:55 -0400

    commit de render

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,10 +8,12 @@ import cors  from 'cors'
 
 const app = express()
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}))
+// app.use(cors({
+//     origin: 'http://localhost:5173',
+//     credentials: true
+// }))
+app.use(cors())
+
 app.use(morgan('dev'));
 app.use(express.json())
 app.use(cookieParser())
--- Backend/src/db.js
+++ Backend/src/db.js
@@ -1,11 +1,18 @@
-import mongoose from  'mongoose';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-export const connectDB = async()=>{
-    try{
-        await mongoose.connect('mongodb://localhost:27017/merndbBACKFRONT');
+// Cargar las variables de entorno
+dotenv.config();
+
+export const connectDB = async () => {
+    try {
+        const mongoURI = process.env.mongodb_URL; // Obtener la URL de la base de datos desde la variable de entorno
+        if (!mongoURI) {
+            throw new Error('La variable de entorno mongodb_URL no está definida');
+        }
+        await mongoose.connect(mongoURI);  // Conectar con la base de datos
         console.log(">>> DB conectada");
+    } catch (err) {
+        console.error("Error de conexión a la DB: ", err.message);
     }
-    catch(err){
-        console.error(err.message);
-    }
-}
+};
