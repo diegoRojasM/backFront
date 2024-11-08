6e9e99ba4206a944dce753c1ff74c1375e779b37
diegoRojasM <diegohelfo@gmail.com>
2024-11-08 01:23:55 -0400

    actualizado la origin cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,24 +1,23 @@
-import express from 'express'
-import morgan from  'morgan'
-import authRoutes from  './routes/auth.routes.js'
-import cookieParser from 'cookie-parser'
-import taskRoutes from  './routes/task.routes.js'
-import cors  from 'cors'
+import express from 'express';
+import morgan from 'morgan';
+import authRoutes from './routes/auth.routes.js';
+import cookieParser from 'cookie-parser';
+import taskRoutes from './routes/task.routes.js';
+import cors from 'cors';
 
+const app = express();
 
-const app = express()
-
-// app.use(cors({
-//     origin: 'http://localhost:5173',
-//     credentials: true
-// }))
-app.use(cors())
+// Configurar CORS para permitir solicitudes desde el frontend en Vercel con credenciales
+app.use(cors({
+  origin: 'https://backfront-1.onrender.com', // Reemplaza con la URL de tu frontend
+  credentials: true, // Permitir cookies o credenciales
+}));
 
 app.use(morgan('dev'));
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
-app.use('/api',authRoutes)
-app.use('/api',taskRoutes)
+app.use('/api', authRoutes);
+app.use('/api', taskRoutes);
 
-export default app
+export default app;
